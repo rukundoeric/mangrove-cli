6a13517a4d7c13a921bc7547adf448f09b752316
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-03-14 23:42:12 +0200

    generate Object model

--- templates/init-templates/javascript/mangrove/connection/index.js
+++ templates/init-templates/javascript/mangrove/connection/index.js
@@ -1,14 +1,14 @@
-const Mangrove = require('mangrove');
-const dotenv = require('dotenv');
+const Mangrove = require('mangrove')
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
-const connection = {};
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config')[env];
+const connection = {}
+const env = process.env.NODE_ENV || 'development'
+const config = require('./config')[env]
 
-const mangrove = new Mangrove(config);
+const mangrove = new Mangrove(config)
 
-connection.mangrove = mangrove;
+connection.mangrove = mangrove
 
-module.exports = connection;
+module.exports = connection
